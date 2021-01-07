5b21fe46c763fead4cdeb569c91a5cbb20cd7dfe
idan chen <idanwork22@gmail.com>
2021-01-07 22:13:51 +0200

    MacBook first changes

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     window = Tk()
     window.mainloop()
+    # idan chen macbook was here
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Gali chen')
+    print_hi('idan chen')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
