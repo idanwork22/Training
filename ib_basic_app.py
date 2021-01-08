6b8e5ff05495d796fd1c0bf51ad273c519b5520d
Idan Chen <idanwork22@gmail.com>
2021-01-09 00:04:24 +0200

    Getting historical data (multiple tickers)
    section 20

--- ib_basic_app.py
+++ ib_basic_app.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-IBAPI - Getting historical data intro
-
-@author: Mayank Rasu (http://rasuquant.com/wp/)
+IBAPI - Getting historical data (multiple tickers)
 """
 
 from ibapi.client import EClient
@@ -13,6 +11,9 @@ import time
 
 
 class TradingApp(EWrapper, EClient):
+    """
+    TradingApp class
+    """
     def __init__(self):
         EClient.__init__(self, self)
 
@@ -20,38 +21,99 @@ class TradingApp(EWrapper, EClient):
         print("HistoricalData. ReqId:", reqId, "BarData.", bar)
 
 
+def createConnection():
+    """
+    This function creates connection with the server
+    127.0.0.1 - the localhost IP
+    7497 - the socket port
+    clientId = the client id (can be any number)
+    :return: return connection type Wrapper and EClient
+    """
+    app = TradingApp()
+    app.connect("127.0.0.1", 7497, clientId=1)
+    return app
+
+
 def websocket_con():
     app.run()
 
 
-app = TradingApp()
-app.connect("127.0.0.1", 7497, clientId=1)
+# open connection - global
+app = createConnection()
 
-# starting a separate daemon thread to execute the websocket connection
-con_thread = threading.Thread(target=websocket_con, daemon=True)
-con_thread.start()
-time.sleep(1)  # some latency added to ensure that the connection is established
+
+def main():
+    """
+    This is the main function
+    """
+    # open connection at  app = createConnection()
+    # starting a separate daemon thread to execute the websocket connection
+    con_thread = threading.Thread(target=websocket_con, daemon=True)
+    con_thread.start()
+    time.sleep(1)  # some latency added to ensure that the connection is established
+
+    # The symbols
+    tickers = ["FB", "AMZN", "INTC"]
+    get_data_tickers(tickers)
+
 
 # creating object of the Contract class - will be used as a parameter for other function calls
-contract = Contract()
-contract.symbol = "AAPL"
-contract.secType = "STK"
-contract.currency = "USD"
-contract.exchange = "ISLAND"
+def usTechStk(symbol, sec_type="STK", currency="USD", exchange="ISLAND"):
+    """
+    Create contract with the given parameters
+    :param symbol: The stoke symbol
+    :param sec_type: always STK
+    :param currency: always USD
+    :param exchange: always ISLAND
+    :return: returns connection
+    """
+    contract = Contract()
+    contract.symbol = symbol
+    contract.secType = sec_type
+    contract.currency = currency
+    contract.exchange = exchange
+    return contract
 
-# https://interactivebrokers.github.io/tws-api/classIBApi_1_1EClient.html#aad87a15294377608e59aec1d87420594 - for
-# more info
-app.reqHistoricalData(reqId=1,
-                      contract=contract,
-                      endDateTime='',  # the empty string indicates current present moment
-                      durationStr='3 M',  # The amount of time (or Valid Duration String units) to go back from the
-                      # request's given end date and time.
-                      barSizeSetting='5 mins',  # The data's granularity or Valid Bar Sizes
-                      whatToShow='MIDPOINT',  # The type of data to retrieve.
-                      useRTH=1,  # Whether (1) or not (0) to retrieve data generated only within Regular Trading
-                      # Hours (RTH)
-                      formatDate=1,  # set to 1 to obtain the bars' time as yyyyMMdd HH:mm:ss
-                      keepUpToDate=0,  # Whether a subscription is made to return updates of unfinished real time
-                      # bars as they are available (True), or all data is returned on a one-time basis (False)
-                      chartOptions=[])  # EClient function to request contract details
-time.sleep(5)  # some latency added to ensure that the contract details request has been processed
+
+def histData(req_num, contract, duration, candle_size):
+    """
+
+    :param req_num: request number - every symnol - different reqId
+    :param contract: given from usTechStk function
+    :param duration: The amount of time to go back from
+    :param candle_size: The canndle size
+    :return: prints the data
+    """
+    # https://interactivebrokers.github.io/tws-api/classIBApi_1_1EClient.html#aad87a15294377608e59aec1d87420594
+    # - for more info
+    app.reqHistoricalData(reqId=req_num,
+                          contract=contract,
+                          endDateTime='',  # the empty string indicates current present moment
+                          durationStr=duration,
+                          # The amount of time (or Valid Duration String units) to go back from the
+                          # request's given end date and time.
+                          barSizeSetting=candle_size,  # The data's granularity or Valid Bar Sizes
+                          whatToShow='ADJUSTED_LAST',  # The type of data to retrieve.
+                          useRTH=1,  # Whether (1) or not (0) to retrieve data generated only within Regular Trading
+                          # Hours (RTH)
+                          formatDate=1,  # set to 1 to obtain the bars' time as yyyyMMdd HH:mm:ss
+                          keepUpToDate=0,  # Whether a subscription is made to return updates of unfinished real time
+                          # bars as they are available (True), or all data is returned on a one-time basis (False)
+                          chartOptions=[])  # EClient function to request contract details
+    # some latency added to ensure that the contract details request has been processed
+
+
+def get_data_tickers(tickers):
+    """
+    for every ticker in the arry - the function calls histData,
+    and between calls - there is a sleep time of 5 second - for Preventing confusion
+    :param tickers: arry of symbols - type str
+    :return: Only call other function
+    """
+    for ticker in tickers:
+        histData(tickers.index(ticker), usTechStk(ticker), '1 D', '5 mins')
+        time.sleep(1)  # some latency added to ensure that the contract details request has been processed
+
+
+if __name__ == '__main__':
+    main()
