943c6b7b0db1dc7ec3274231badd3cdc81764415
Idan Chen <idanwork22@gmail.com>
2021-01-08 18:17:35 +0200

    Getting Contract Info
    section 15 - apple

--- ib_basic_app.py
+++ ib_basic_app.py
@@ -6,6 +6,7 @@ IBAPI - EClient and EWrapper classes intro
 
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
 
 
 class TradingApp(EWrapper, EClient):
@@ -15,10 +16,21 @@ class TradingApp(EWrapper, EClient):
     def error(self, reqId, errorCode, errorString):
         print("Error {} {} {}".format(reqId, errorCode, errorString))
 
+    def contractDetails(self, reqId, contractDetails):
+        print("redID: {}, contract:{}".format(reqId, contractDetails))
+
 
 def main():
     app = TradingApp()
     app.connect("127.0.0.1", 7497, clientId=1)
+
+    contract = Contract()  # Create contract
+    contract.symbol = "AAPL"  # symbol
+    contract.secType = "STK"  # Security type
+    contract.currency = "USD"  # Currency
+    contract.exchange = "SMART"  # necessary exchange
+
+    app.reqContractDetails(100, contract)
     app.run()
 
 
